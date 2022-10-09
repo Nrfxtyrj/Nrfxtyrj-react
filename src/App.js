496d6a2ddfa2f18367268e6bea32c2f43e4e545f
Nrfxtyrj <antontkachenko162@mail.com>
2022-10-09 22:27:26 +0300

    add modified:   src/App.css         modified:   src/App.js         modified:   src/aboutme/Aboutme.css         modified:   src/aboutme/Aboutme.js         modified:   src/projects/Projects.css         modified:   src/projects/Projects.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 
 import Projects from './projects/Projects.js';
-import About from './aboutme/Aboutme.js';
-import React, {useState } from "react";
+import Aboutme from './aboutme/Aboutme.js';
 import './App.css';
 
 const App = () => {
 
-  
-
-  const [modalActive, setModalActive] = useState(true);
   return (
     <div className='App'>
       <div className='container'><div><h1 className='Name'>Nrfxtyrj</h1></div><div><h1 className='Name-plus'>_</h1></div></div>
             <ul>
-            <li><button className='open-bth' onClick={() => setModalActive(true)}>обо мне</button></li>
-            <li><button className='open-bth' onClick={() => setModalActive(true)}>мои проекты</button>  </li>
-            <li><a className='open-bth' href="https://github.com/Nrfxtyrj" target="_blank">__GitHub__</a></li>
+            <li className='link'><Aboutme /></li>
+            <li className='link'><Projects /> </li>
+            <li className='link'><a className='open-bth' href="https://github.com/Nrfxtyrj" target="_blank">__GitHub__</a></li>
           </ul>
-          <Projects active={modalActive}
-          setActive={setModalActive} />
+          
     </div>
   );
 }
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,32 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Projects.css';
+class Projects extends Component {
+    
+    state = {
+        isOpen: false
+    }
 
-const Projects = ({active, setActive}) => {
-    return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)} >
-        <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
-            <ul>
-            <li><div><a className='open-bth' href="https://nrfxtyrj.github.io/retrogame.github.io/menu/retromenu.html" target="_blank">RETRO GAME</a></div>
-        <div><p>Экзаменационная работа сделанная виде двух аркадных игр. С применением технологий HTML, CSS, JS</p></div>
-        <div><a  href="https://nrfxtyrj.github.io/retrogame.github.io/menu/retromenu.html" target="_blank">Перейти напрямую к работе</a>
-        <a  href="https://nrfxtyrj.github.io/retrogame.github.io" target="_blank">Перейти в репозиторй работы</a></div></li>
-        <li><div><a className='open-bth' href="https://nrfxtyrj.github.io/animewebsitej.github.io/mywork.html" target="_blank">ANIME WEB SITE</a>
-        <div><p> Самостоятельная работа для применение знайний. С применением технологий HTML, CSS, bootstrap</p></div>
-        <a  href="https://nrfxtyrj.github.io/animewebsitej.github.io/mywork.html" target="_blank">Перейти напрямую к работе</a>
-        <a  href="https://nrfxtyrj.github.io/animewebsitej.github.io" target="_blank">Перейти в репозиторй работы</a></div></li>
-        <li><div><a className='open-bth' href="https://nrfxtyrj.github.io/animewebsitej.github.io" target="_blank">Bootstrap WEB SITE</a></div>
-        <div><p> Готовая страница для bootstrap . С применением технологий HTML, CSS, bootstrap</p></div>
-        <div><a  href="https://nrfxtyrj.github.io/animewebsitej.github.io" target="_blank">Перейти в репозиторй работы</a></div></li>
-        <li><div><a className='open-bth' href="https://github.com/Nrfxtyrj/github-exam" target="_blank"> WEB SITE EXAM</a></div>
-        <div><p>Экзаменационная работа сделанная виде двух аркадных игр. С применением технологий HTML, CSS</p></div>
-        <div><a  href="https://github.com/Nrfxtyrj/github-exam" target="_blank">Перейти в репозиторй работы</a></div></li>
-        <li><div><a className='open-bth' href="https://github.com/Nrfxtyrj/Work-of-task" target="_blank"> WORK OF TASK</a></div>
-        <div><p>Самостоятельная работа  сделанная виде тестового задания. С применением технологий HTML, CSS</p></div>
-        <div><a  href="https://github.com/Nrfxtyrj/Work-of-task" target="_blank">Перейти в репозиторй работы</a></div></li>
-        </ul>
+    openModalHandler = () => {
+        this.setState({
+            isOpen: true
+        })
+    }
+    
+    closeModalHandler = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+    
+    render() {
+        return ( 
+            <div className='modal-container' >
+            <button onClick={this.openModalHandler} className="open-modal-btn">мои проекты</button>
+            <div className={`overlay animated ${this.state.isOpen ? "show" : null}`}>
+            <div className="modal-projects">
+            <svg onClick={this.closeModalHandler} height="200" viewBox="0 0 200 200" width="200" >
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <div className='main-block'>
+                <div className='block' ><div><a href="https://nrfxtyrj.github.io/retrogame.github.io/menu/retromenu.html" target="_blank">RETRO GAME</a></div>
+                    <div className='description'><p >Экзаменационная работа сделанная виде двух аркадных игр. С применением технологий HTML, CSS, JS</p></div>
+                    <div className='link-app'>
+                        <div>
+                        <div><a href="https://github.com/Nrfxtyrj/retrogame.github.io" target="_blank">&#128279;</a></div>
+                        <div><a href="https://nrfxtyrj.github.io/retrogame.github.io/menu/retromenu.html" target="_blank"> ▶</a></div>
+                        </div>
+                        </div>
+                    </div>
+                <div className='block' ><div><a  href="https://nrfxtyrj.github.io/animewebsitej.github.io/mywork.html" target="_blank">ANIME WEB SITE</a>
+                    <div className='description'><p> Самостоятельная работа для применение знайний. С применением технологий HTML, CSS, bootstrap</p></div>
+                    <div className='link-app'>
+                        <div>
+                        <div><a href="https://github.com/Nrfxtyrj/animewebsitej.github.io" target="_blank">&#128279;</a></div>
+                        <div><a href="https://nrfxtyrj.github.io/retrogame.github.io/menu/retromenu.html" target="_blank"> ▶</a></div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='block' ><div><a  href="https://nrfxtyrj.github.io/animewebsitej.github.io" target="_blank">Bootstrap WEB SITE</a></div>
+                    <div className='description'><p> Готовая страница для bootstrap . С применением технологий HTML, CSS, bootstrap</p></div>
+                    <div><a href="https://github.com/Nrfxtyrj/bootstrap-task" target="_blank">&#128279;</a></div></div>
+                <div className='block' ><div><a  href="https://github.com/Nrfxtyrj/github-exam" target="_blank"> WEB SITE EXAM</a></div>
+                    <div className='description'><p>Экзаменационная работа сделанная виде двух аркадных игр. С применением технологий HTML, CSS</p></div>
+                    <div><a href="https://github.com/Nrfxtyrj/github-exam" target="_blank">&#128279;</a></div></div>
+                <div className='block' ><div><a  href="https://github.com/Nrfxtyrj/Work-of-task" target="_blank"> WORK OF TASK</a></div>
+                    <div className='description'><p>Самостоятельная работа сделанная виде тестового задания. С применением технологий HTML, CSS</p></div>
+                    <div><a href="https://github.com/Nrfxtyrj/Work-of-task" target="_blank">&#128279;</a></div></div>
+                </div>
+            </div>
+            </div>
         </div>
-        </div>
-    );
-};
-
-export default Projects;
+        )
+    }
+}
+export default Projects ;
